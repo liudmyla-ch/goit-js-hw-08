d0ee7a1789b20ac266eea29725016c655e196013
liudmyla-ch <110854340+liudmyla-ch@users.noreply.github.com>
2023-01-11 01:43:20 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Change code below this line
 
 console.log(galleryItems);
 console.log(SimpleLightbox);
+
+
+
+const list = document.querySelector(".gallery");
+const imagesMarkup = createImageMarkup(galleryItems);
+list.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createImageMarkup(images) {
+  return images.map(({ description, original, preview }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+    </a>
+    </div>`;
+  })
+  .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); 
+});
+
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,40 @@
 import Player from '@vimeo/player';
-console.log(Player)
+console.log(Player);
+import throttle from 'lodash.throttle';
+console.log(throttle);
+
+
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(function(currentTime) {
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime.seconds));
+        console.log('played the video!');
+    },1000));
+
+    
+    const timeToPlay = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+    console.log(timeToPlay);
+      
+
+    player.setCurrentTime(timeToPlay).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+
+
+ 
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
